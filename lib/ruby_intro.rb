34c125f7f8196b5f00f9323ae8491ecd2855549e
EC2 Default User <ec2-user@ip-172-31-23-251.us-east-2.compute.internal>
2020-09-09 04:33:55 +0000

    hw1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,49 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0){|sum,x| sum+x}
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+	return 0 if arr.empty?
+	return arr[0] if arr.length == 1
+	return arr.sort[-2..-1].inject(:+)
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.empty? or arr.length == 1
+  (arr.empty? && n.zero?) || arr.permutation(2).any? { |a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+	/^[b-df-hj-np-tv-z]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+	return true if s == "0"
+	/^[10]*00$/.match(s) != nil
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+	attr_accessor :price
+	
+	def initialize(isbn, price)
+		raise ArgumentError, 
+			"ISBN number is the empty string or the price is less than or equal to zero" if isbn.empty?  or price <= 0
+		@isbn = isbn
+		@price = price
+	end
+
+	def price_as_string
+		sprintf("$%2.2f", @price)
+	end
 end
